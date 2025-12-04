064dc60f24e760ce06bd4cedeadafec61925727c
KarimOsama99 <ko5418301@gmail.com>
2025-12-04 11:18:33 +0200

    particles

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,6 +33,113 @@
 	
 	headerStyle();
 
+	if (document.getElementById('particles-banner')) {
+		particlesJS('particles-banner', {
+		"particles": {
+			"number": {
+			"value": 80,
+			"density": {
+				"enable": true,
+				"value_area": 800
+			}
+			},
+			"color": {
+			"value": "#ffffff"
+			},
+			"shape": {
+			"type": "circle",
+			"stroke": {
+				"width": 0,
+				"color": "#000000"
+			},
+			"polygon": {
+				"nb_sides": 5
+			}
+			},
+			"opacity": {
+			"value": 0.5,
+			"random": false,
+			"anim": {
+				"enable": false,
+				"speed": 1,
+				"opacity_min": 0.1,
+				"sync": false
+			}
+			},
+			"size": {
+			"value": 8,        // الحجم الأساسي (جرب: 5-15)
+			"random": true,    // أحجام عشوائية
+			"anim": {
+				"enable": true,  // تحريك الحجم (كبير/صغير)
+				"speed": 40,     // سرعة التحريك
+				"size_min": 2,   // أصغر حجم ممكن
+				"sync": false
+			}
+			},
+			"line_linked": {
+			"enable": true,
+			"distance": 150,
+			"color": "#ffffff",
+			"opacity": 0.4,
+			"width": 1
+			},
+			"move": {
+			"enable": true,
+			"speed": 6,
+			"direction": "none",
+			"random": false,
+			"straight": false,
+			"out_mode": "out",
+			"bounce": false,
+			"attract": {
+				"enable": false,
+				"rotateX": 600,
+				"rotateY": 1200
+			}
+			}
+		},
+		"interactivity": {
+			"detect_on": "canvas",
+			"events": {
+			"onhover": {
+				"enable": true,
+				"mode": "grab"
+			},
+			"onclick": {
+				"enable": true,
+				"mode": "push"
+			},
+			"resize": true
+			},
+			"modes": {
+			"grab": {
+				"distance": 140,
+				"line_linked": {
+				"opacity": 1
+				}
+			},
+			"bubble": {
+				"distance": 400,
+				"size": 40,
+				"duration": 2,
+				"opacity": 8,
+				"speed": 3
+			},
+			"repulse": {
+				"distance": 200,
+				"duration": 0.4
+			},
+			"push": {
+				"particles_nb": 4
+			},
+			"remove": {
+				"particles_nb": 2
+			}
+			}
+		},
+		"retina_detect": true
+		});
+	}
 
 	//Submenu Dropdown Toggle
 	if($('.main-header li.dropdown ul').length){
